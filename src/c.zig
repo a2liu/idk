3396ba0d84d5767fd675161ffe27453ee407e2e8
Albert Liu <albertymliu@gmail.com>
2022-04-03 17:24:27 -0400

    oops

---
+++ src/c.zig
@@ -0,0 +1,11 @@
+usingnamespace @cImport({
+    @cUndef("__cplusplus");
+    @cInclude("vulkan/vulkan.h");
+
+    @cDefine("CIMGUI_DEFINE_ENUMS_AND_STRUCTS", "1");
+    @cInclude("cimgui.h");
+
+    @cDefine("IMGUI_IMPL_API", "");
+    @cDefine("NULL", "((void*) 0)");
+    @cInclude("imgui_impl_render.h");
+});
