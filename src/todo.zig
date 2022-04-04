219a2da18165ccab137c225bf58b0d939653e3f8
Albert Liu <albertymliu@gmail.com>
2022-04-04 02:40:32 -0400

    blarg

--- src/todo.zig
+++ src/todo.zig
@@ -15,6 +15,15 @@ const TodoItem = struct {
 // thread.
 var items = ArrayList(TodoItem).init(alloc.Global);
 
+fn textCallback(data: [*c]c.ImGuiInputTextCallbackData) callconv(.C) c_int {
+    _ = data;
+
+    // https://github.com/ocornut/imgui/blob/master/misc/cpp/imgui_stdlib.cpp
+    // https://github.com/ocornut/imgui/blob/1ba2905017ea81545b2fd1d45fee093a4982d9b1/imgui.h#L2071
+
+    return 0;
+}
+
 pub fn todoApp(is_open: *bool) !void {
     c.igSetNextWindowSize(.{ .x = 300, .y = 400 }, c.ImGuiCond_FirstUseEver);
 
@@ -24,15 +33,26 @@ pub fn todoApp(is_open: *bool) !void {
     if (c.igButton("Add a todo item", .{ .x = 0, .y = 0 })) {
         var newItem = TodoItem{};
         try newItem.name.ensureUnusedCapacity(64);
+
+        const allocated = newItem.name.allocatedSlice();
+        @memset(allocated.ptr, 0, allocated.len);
+
         try items.append(newItem);
     }
 
     for (items.items) |item, index| {
-        const name = item.name;
+        var name = item.name;
 
         c.igPushID_Int(cast(c_int, index) catch @panic("wtf"));
 
-        _ = c.igInputText("##", name.items.ptr, name.items.len, 0, null, null);
+        _ = c.igInputText(
+            "##",
+            name.items.ptr,
+            name.capacity,
+            0,
+            textCallback,
+            &name,
+        );
 
         c.igPopID();
     }
