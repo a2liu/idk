f973e997510218dd23dfa4e28dfd63a2b4948266
Albert Liu <albertymliu@gmail.com>
2022-04-03 21:10:09 -0400

    blarg

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,27 @@ const glfw = @import("glfw");
 const render = @import("render/mod.zig");
 const c = @import("c.zig");
 
+const app_name = "Dear ImGui GLFW+Vulkan example";
+
 pub fn main() !void {
     try glfw.init(.{});
 
-    const result = c.cpp_main();
+    const width = 1280;
+    const height = 720;
+    const window = try glfw.Window.create(width, height, app_name, null, null, .{
+        .client_api = .no_api,
+    });
+
+    const handle = @ptrCast(*c.struct_GLFWwindow, window.handle);
+
+    c.cpp_init(handle);
+
+    while (!window.shouldClose()) {
+        c.cpp_loop(handle);
+    }
+
+    const result = c.cpp_teardown(handle);
+
     if (result != 0) {
         const pressed = c.igSmallButton("Hello");
         _ = pressed;
