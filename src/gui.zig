8a31826ea64db2c8ff189b78c5b90dec6de953ba
Albert Liu <albertymliu@gmail.com>
2022-04-07 00:05:57 -0400

    blarg

--- src/gui.zig
+++ src/gui.zig
@@ -7,7 +7,11 @@ const c = @import("c.zig");
 // const Key = glfw.Key;
 
 pub fn Text(comptime format: []const u8, args: anytype) void {
-    const allocResult = std.fmt.allocPrint(alloc.Frame, format, args);
+    var _temp = alloc.Temp.init();
+    const temp = _temp.allocator();
+    defer _temp.deinit();
+
+    const allocResult = std.fmt.allocPrint(temp, format, args);
     const s = allocResult catch @panic("failed to print");
     c.igTextUnformatted(s.ptr, s.ptr + s.len);
 }
