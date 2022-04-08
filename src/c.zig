fa8b98fab995817b6b65b39669bacb2d14b4c530
Albert Liu <albertymliu@gmail.com>
2022-04-07 21:34:04 -0400

    blarg

--- src/c.zig
+++ src/c.zig
@@ -1,4 +1,7 @@
-usingnamespace @cImport({
+const std = @import("std");
+
+usingnamespace raw;
+const raw = @cImport({
     @cUndef("__cplusplus");
     @cInclude("vulkan/vulkan.h");
 
@@ -10,3 +13,14 @@ usingnamespace @cImport({
     @cInclude("imgui_impl_render.h");
     @cInclude("imgui_impl_platform.h");
 });
+
+pub const str_z = [*:0]const u8;
+pub const str = [*c]const u8;
+
+pub fn vkErr(err: raw.VkResult) callconv(.C) void {
+    if (err == 0) return;
+
+    std.debug.print("[vulkan] Error: VkResult = {}\n", .{err});
+    if (err < 0)
+        @panic("[vulkan] aborted");
+}
